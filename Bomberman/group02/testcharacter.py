bbbd697ba7d551f0ff484fd0f5ba9ae8af655d3b
Craigie <cacraigie@wpi.edu>
2021-03-07 13:43:43 -0500

    working BFS to goal

--- Bomberman/group02/testcharacter.py
+++ Bomberman/group02/testcharacter.py
@@ -7,6 +7,124 @@ from colorama import Fore, Back
 
 class TestCharacter(CharacterEntity):
 
+    char = CharacterEntity
+    char_x = 0
+    char_y = 0
+
     def do(self, wrld):
         # Your code here
-        pass
+        
+        print("\n\n")
+        next_move = self.nextMoveToExit(wrld)
+        self.moveChar(wrld, next_move[0], next_move[1])
+        print("Monsters Located at: "+str(self.locateMonsters(wrld)))
+
+
+    def nextMoveToExit(self, wrld):
+        # Simple BFS to poll the exit
+        q = [(self.char_x, self.char_y)]
+        visited = []
+        path = {}
+
+        while q:
+            cur = q.pop(0)
+            visited.append(cur)
+
+            if wrld.exit_at(cur[0],cur[1]):
+                print("EXIT FOUND: (" + str(cur[0])+", "+str(cur[1])+")")
+                # Find next movement
+                tmp = cur
+                while path[tmp] != (self.char_x, self.char_y):
+                    tmp = path[tmp]
+                return (tmp[0]-self.char_x, tmp[1]-self.char_y)
+                
+            for neigh in self.getNeighbors(wrld, cur[0], cur[1]):
+                if neigh not in visited and neigh not in q:
+                    q.append(neigh)
+                    path[neigh] = cur
+
+        print("!!! No path to EXIT detected !!!")
+        return (0,0)
+
+
+    def locateMonsters(self, wrld):
+        # Simple BFS to poll the monsters
+        monsters = []
+        q = [(self.char_x, self.char_y)]
+        visited = []
+        path = {}
+
+        while q:
+            cur = q.pop(0)
+            visited.append(cur)
+            # If cur holds a monster log its location
+            if wrld.monsters_at(cur[0], cur[1]) != None:
+                dist = 1
+                tmp = cur
+                while tmp != (self.char_x, self.char_y):
+                    dist = dist + 1
+                    tmp = path[tmp]
+                monsters.append( ((cur[0], cur[1]), dist) )
+
+            # break early if both monsters are found
+            if len(monsters) >= 2:
+                return monsters
+
+            for neigh in self.getNeighbors(wrld, cur[0], cur[1]):
+                if neigh not in visited and neigh not in q:
+                    q.append(neigh)
+                    path[neigh] = cur
+
+        return monsters
+
+
+    def isCoordinateValid(self, wrld, x, y):
+        is_valid = True
+
+        # Bounds check
+        if x >= wrld.width() or x < 0:
+            return False
+        if y >= wrld.height() or y < 0:
+            return False
+        if wrld.wall_at(x, y):
+            is_valid = False
+
+        return is_valid
+
+
+    def getNeighbors(self, wrld, x, y):
+        offsets = [-1, 0, 1]
+        neighbors = []
+
+        for dx in offsets:
+            for dy in offsets:
+                # dx,dy == 0,0 is the searchng node
+                if not (dx == 0 and dy == 0):
+                    if self.isCoordinateValid(wrld, x+dx, y+dy):
+                        neighbors.append((x+dx, y+dy))
+
+        return neighbors
+
+
+    def moveChar(self, wrld, dx, dy):
+        # X check
+        if dx > 1:
+            dx = 1
+        elif dx < -1:
+            dx = -1
+        # Y check
+        if dy > 1:
+            dy = 1
+        elif dy < -1:
+            dy = -1
+
+        # Bounds check
+        if not self.isCoordinateValid(wrld, self.char_x+dx, self.char_y+dy):
+            print("Invalid Move Detected")
+            dx = 0
+            dy = 0
+
+        self.char_x = self.char_x + dx
+        self.char_y = self.char_y + dy
+        print("I am now at ("+str(self.char_x)+", "+str(self.char_y)+")")
+        self.move(dx, dy)
